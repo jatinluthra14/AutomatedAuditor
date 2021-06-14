143567fa7764463ec433490ab53efe1a9c9ae95d
Jatin Luthra <jatinluthra14@gmail.com>
2021-06-14 18:22:04 +0530

    Platforms

--- audit.py
+++ audit.py
@@ -2,10 +2,12 @@ import sys
 from models.s3bucket import S3Bucket
 from getpass import getpass
 
+platforms = ['aws', 'gcp']
+
 
 def print_usage():
-    print(f"Usage: python {__file__} <bucket_name>")
-    print(f"Example: python {__file__} test_bucket")
+    print(f"Usage: python {__file__} <platform(aws|gcp)> <bucket_name>")
+    print(f"Example: python {__file__} aws test_bucket")
     exit(1)
 
 
@@ -24,9 +26,17 @@ def input_creds() -> tuple[str, str]:
 
 if __name__ == '__main__':
     if len(sys.argv) < 2:
+        print("Error: Less Number of Arguments!")
+        print_usage()
+    elif len(sys.argv) < 3:
         bucket_name = ""
     else:
-        bucket_name = sys.argv[1].strip()
+        bucket_name = sys.argv[2].strip()
+
+    platform = sys.argv[1].lower().strip()
+    if platform not in platforms:
+        print("Error: Invalid Platform!")
+        print_usage()
 
     creds = input_creds()
     if creds[0] and creds[1]:
