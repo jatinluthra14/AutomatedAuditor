16af4dbe64a161cb79482c49660901d3bc9d1880
Jatin Luthra <jatinluthra14@gmail.com>
2021-06-17 20:17:39 +0530

    Azure Platform

--- audit.py
+++ audit.py
@@ -1,8 +1,9 @@
 from models.s3bucket import S3Bucket
 from models.gcpbucket import GCPBucket
+from models.azblob import AZBlob
 import argparse
 
-platforms = ['aws', 'gcp']
+platforms = ['aws', 'gcp', 'az']
 
 
 def init_s3bucket(args: argparse.Namespace) -> None:
@@ -18,16 +19,25 @@ def init_gcpbucket(args: argparse.Namespace) -> None:
     bucket.start()
 
 
+def init_azblob(args: argparse.Namespace) -> None:
+    blob = AZBlob(container_name=args.container_name, tenant_id=args.az_creds[0], client_id=args.az_creds[1], client_secret=args.az_creds[2], subscription_id=args.az_creds[3])
+    blob.start()
+
+
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='A Cross Cloud Platform Automated Auditor')
     parser.add_argument('platform', help='The Platform you want to audit', choices=platforms, type=str.lower)
-    parser.add_argument('-b', '--bucket-name', default='', metavar='BucketName', help='The Name of Bucket to Audit (Exclude to Audit All)')
+    parser.add_argument('-b', '--bucket-name', default='', metavar='BucketName', help='The Name of Bucket/Blob to Audit (Exclude to Audit All)')
+    parser.add_argument('-c', '--container-name', default='', metavar='ContainerName', help='The Name of Container to Audit (Azure Only, Specify Blob using -b if needed)')
     parser.add_argument('--aws-creds', default='', nargs=2, metavar=('AWS_Access_Key_ID', 'AWS_Secret_Access_Key'), help='AWS ID and Secret key (Space Separated)')
     parser.add_argument('--gcp-creds', default='', metavar='JSON_Path', help='GCP Creds JSON File Path')
+    parser.add_argument('--az-creds', default='', nargs=4, metavar=('AZ_Tenant_ID', 'AZ_Client_ID', 'AZ_Client_Secret', 'AZ_Subcription_ID'), help='Azure Service Principal Credentials and an Active Subcription ID (Space Separated)')
     args = parser.parse_args()
 
     if args.platform == 'aws':
         init_s3bucket(args)
     elif args.platform == 'gcp':
         init_gcpbucket(args)
+    elif args.platform == 'az':
+        init_azblob(args)
--- models/azblob.py
+++ models/azblob.py
@@ -1,4 +1,6 @@
 from azure.identity import ClientSecretCredential
+from utils.cprint import cprint
+from azure.core.exceptions import ClientAuthenticationError
 
 
 class AZBlob():
@@ -9,4 +11,20 @@ class AZBlob():
         self.client_secret = client_secret
         self.subscription_id = subscription_id
 
-        self.credential = ClientSecretCredential(tenant_id=self.tenant_id, client_id=self.client_id, client_secret=self.client_secret)
+        self.credential: ClientSecretCredential
+
+    def validate_creds(self) -> bool:
+        try:
+            self.credential = ClientSecretCredential(tenant_id=self.tenant_id, client_id=self.client_id, client_secret=self.client_secret)
+            token = self.credential._request_token('https://storage.azure.com/.default')
+            print(token)
+            if token:
+                return True
+        except ClientAuthenticationError as e:
+            cprint(e, error=True)
+        return False
+
+    def start(self) -> None:
+        if not self.validate_creds():
+            cprint("Error in Credentials", error=True)
+            return
