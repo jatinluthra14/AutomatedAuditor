9d380b21e9fff3d34839a4f9d6c395ce2162f9f8
Jatin Luthra <jatinluthra14@gmail.com>
2021-06-18 15:22:05 +0530

    Storage Account Validation

--- audit.py
+++ audit.py
@@ -20,7 +20,7 @@ def init_gcpbucket(args: argparse.Namespace) -> None:
 
 
 def init_azblob(args: argparse.Namespace) -> None:
-    blob = AZBlob(container_name=args.container_name, tenant_id=args.az_creds[0], client_id=args.az_creds[1], client_secret=args.az_creds[2], subscription_id=args.az_creds[3])
+    blob = AZBlob(container_name=args.bucket_name, storage_acct_name=args.storage_acct_name, tenant_id=args.az_creds[0], client_id=args.az_creds[1], client_secret=args.az_creds[2], subscription_id=args.az_creds[3])
     blob.start()
 
 
@@ -28,8 +28,8 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='A Cross Cloud Platform Automated Auditor')
     parser.add_argument('platform', help='The Platform you want to audit', choices=platforms, type=str.lower)
-    parser.add_argument('-b', '--bucket-name', default='', metavar='BucketName', help='The Name of Bucket/Blob to Audit (Exclude to Audit All)')
-    parser.add_argument('-c', '--container-name', default='', metavar='ContainerName', help='The Name of Container to Audit (Azure Only, Specify Blob using -b if needed)')
+    parser.add_argument('-b', '--bucket-name', default='', metavar='BucketName', help='The Name of Bucket/Container to Audit (Exclude to Audit All)')
+    parser.add_argument('-s', '--storage-acct-name', default='', metavar='StorageAcctName', help='The Name of Storage Account to Audit (Azure Only, Specify Container using -b if needed)')
     parser.add_argument('--aws-creds', default='', nargs=2, metavar=('AWS_Access_Key_ID', 'AWS_Secret_Access_Key'), help='AWS ID and Secret key (Space Separated)')
     parser.add_argument('--gcp-creds', default='', metavar='JSON_Path', help='GCP Creds JSON File Path')
     parser.add_argument('--az-creds', default='', nargs=4, metavar=('AZ_Tenant_ID', 'AZ_Client_ID', 'AZ_Client_Secret', 'AZ_Subcription_ID'), help='Azure Service Principal Credentials and an Active Subcription ID (Space Separated)')
--- models/azblob.py
+++ models/azblob.py
@@ -1,30 +1,56 @@
-from azure.identity import ClientSecretCredential
 from utils.cprint import cprint
+from utils.loader import Loader
+from azure.identity import ClientSecretCredential
 from azure.core.exceptions import ClientAuthenticationError
+from azure.mgmt.storage import StorageManagementClient
+from azure.storage.blob import ContainerClient
 
 
 class AZBlob():
-    def __init__(self, container_name: str = "", tenant_id: str = "", client_id: str = "", client_secret: str = "", subscription_id: str = "") -> None:
+    def __init__(self, container_name: str = "", storage_acct_name: str = "", tenant_id: str = "", client_id: str = "", client_secret: str = "", subscription_id: str = "") -> None:
         self.container_name = container_name
+        self.storage_acct_name = storage_acct_name
         self.tenant_id = tenant_id
         self.client_id = client_id
         self.client_secret = client_secret
         self.subscription_id = subscription_id
 
         self.credential: ClientSecretCredential
+        self.storage_mgmt: StorageManagementClient
+        self.client: ContainerClient
+        self.storage_accts = list[str]()
+
+        self.loader = Loader()
 
     def validate_creds(self) -> bool:
+        self.loader.load_message('Validating Credentials...')
         try:
             self.credential = ClientSecretCredential(tenant_id=self.tenant_id, client_id=self.client_id, client_secret=self.client_secret)
             token = self.credential._request_token('https://storage.azure.com/.default')
-            print(token)
             if token:
                 return True
         except ClientAuthenticationError as e:
             cprint(e, error=True)
         return False
 
+    def check_storage_acct(self) -> bool:
+        self.loader.load_message('Validating Storage Account...')
+        try:
+            self.storage_mgmt = StorageManagementClient(credential=self.credential, subscription_id=self.subscription_id)
+            self.storage_accts = [str(storage_acct.name) for storage_acct in self.storage_mgmt.storage_accounts.list()]
+            if self.storage_acct_name in self.storage_accts:
+                return True
+        except Exception as e:
+            self.loader.done_message(message=e, status=False)
+        return False
+
     def start(self) -> None:
         if not self.validate_creds():
-            cprint("Error in Credentials", error=True)
+            self.loader.done_message(message="Error in Credentials.", status=False)
             return
+        self.loader.done_message(message="Credentials Validated.", status=True)
+
+        if not self.check_storage_acct():
+            self.loader.done_message(message="Invalid Storage Account Requested!", status=False)
+            return
+        self.loader.done_message(message="Storage Account Validated.", status=True)
